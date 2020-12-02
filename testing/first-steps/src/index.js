d23633661617a2da9e0922f3b559c4160c6494de
Simon Hoyos <shmesa23@gmail.com>
2020-12-02 15:35:33 -0500

    Testing jest matchers

--- testing/first-steps/src/index.js
+++ testing/first-steps/src/index.js
@@ -1,3 +1,21 @@
 exports.sum = (a, b) => {
   return a + b
 }
+
+exports.multiply = (a, b) => {
+  return a * b
+}
+
+exports.person = (name, age) => {
+  return {
+    name,
+    age,
+    greet() {
+      return 'hola mundo!'
+    }
+  }
+}
+
+exports.map = (cb) => {
+  cb()
+}
--- testing/first-steps/src/index.test.js
+++ testing/first-steps/src/index.test.js
@@ -1,9 +1,45 @@
-const { sum } = require('.');
+const { sum, multiply, person, map } = require('.');
 
-it('should add two numbers correctly', () => {
-  const result = sum(1,3)
-  expect(result).toBe(4)
-  expect(sum(2,5)).toBe(7)
-  expect(sum(2834974, 1234871237)).toBe(1237706211)
+describe('sum', () => {
+  it('should add two numbers correctly', () => {
+    const result = sum(1,3)
+    expect(result).toBe(4)
+    expect(sum(2,5)).toBe(7)
+    expect(sum(2834974, 1234871237)).toBe(1237706211)
+  })
 })
 
+describe('multiply', () => {
+  test('should multiply two number correctly', () => {
+    expect(multiply(2,4)).toBe(8)
+  })
+
+  test('should multiply 3 and 5 correctly', () => {
+    expect(multiply(3,5)).toBe(15)
+  })
+})
+
+describe('person', () => {
+  it('should create a person with name maria and age 27', () => {
+    const maria = person('maria', 27)
+
+    expect(maria).toMatchObject({ name: 'maria', age: 27 })
+    // expect(maria.name).toBe('maria')
+    // expect(maria.age).toBe(27)
+  })
+
+  it('should be able to greet', () => {
+    const maria = person('maria', 27)
+
+    expect(maria.greet()).toMatch(/hola/i)
+  })
+})
+
+describe('map', () => {
+  it('should execute callback', () => {
+    const handleSubmit = jest.fn()
+    map(handleSubmit)
+
+    expect(handleSubmit).toHaveBeenCalled()
+  })
+})
