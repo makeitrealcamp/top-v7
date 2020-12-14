ff6cb99c71119d8ab543d46ba3a601272d7da904
Simon Hoyos <shmesa23@gmail.com>
2020-12-14 14:20:13 -0500

    Integration testing

--- testing/tasks/frontend/src/App.test.js
+++ testing/tasks/frontend/src/App.test.js
@@ -1,9 +1,17 @@
 import puppeteer from 'puppeteer'
 
 describe('App', () => {
-  it('should redirect to home after successfully signing up', async () => {
-    const browser = await puppeteer.launch({ headless: false })
+  let browser;
 
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: false })
+  })
+
+  afterAll(async () => {
+    await browser.close()
+  })
+
+  it('should redirect to home after successfully signing up', async () => {
     const page = await browser.newPage()
 
     await page.goto('http://localhost:3000')
@@ -21,6 +29,38 @@ describe('App', () => {
 
     expect(content).toMatch(/No tasks created yet/)
 
-    await browser.close()
+    await page.close()
+  })
+
+  it('should create task and list the user\'s task', async () => {
+    const page = await browser.newPage()
+
+    await page.goto('http://localhost:3000')
+
+    await page.waitForSelector('a[href="/create"]')
+    await page.click('a[href="/create"]')
+
+    await page.waitForSelector('#name')
+    await page.type('#name', 'task 1', { delay: 100 })
+    await page.click('#name ~ button')
+
+    await page.waitForSelector('.success-message')
+    const content = await page.$eval('.success-message', el => el.innerHTML)
+
+    expect(content).toMatch(/task created successfully/i)
+
+    await page.reload()
+    await page.waitForSelector('#name')
+    await page.type('#name', 'task 2', { delay: 100 })
+    await page.click('#name ~ button')
+
+    await page.click('a[href="/"]')
+
+    await page.waitForSelector('.tasks')
+    await page.screenshot({ path: './tasks.png' })
+
+    const tasks = await page.$$('.task')
+
+    expect(tasks).toHaveLength(2)
   })
 })
